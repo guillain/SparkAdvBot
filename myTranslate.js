33b26dd2cc9fc4abb39c63a807a50bc7a76e5bd5
guillain <guillain@gmail.com>
2017-05-06 13:39:37 +0200

    Functions for translation, 1.0.3

--- myTranslate.js
+++ myTranslate.js
@@ -4,19 +4,28 @@
  * @license GPL-3.0
  */
 
+exports.help = function(bot) {
+  var help  = '**Translate** \n\n';
+  help += '@ [translate|t] [lang in] [lang out] [*/phrase] \n';
+  help += '* @ translate en fr I like it! \n';
+  help += '* @ t fr ge rendez-vous demain \n';
+  help += '_lang_: en, es, fr, ge \n\n';
+  bot.say(help);
+}
+
 exports.translate = function (bot, trigger) {
   var tosay = '';
   var translate = require('node-google-translate-skidz');
 
-  if (/help/i.test(trigger.args['1'])) {
-    tosay  = '\\translate [lang in] [lang out] [*/phrase]';
-    tosay += 'lang: en, es, fr, ge';
-    bot.say(tosay);
-  } else {
+  // Remove the first two args
+  trigger.args.splice(0,2);
+  
+  if (/help/i.test(trigger.args['0'])) { module.exports.help(bot); }
+  else {
     var phrase = '';
-    var langIn = trigger.args['1'];
-    var langOut = trigger.args['2'];
-    for (i = 3; i < trigger.args.length; i++) {
+    var langIn = trigger.args['0'];
+    var langOut = trigger.args['1'];
+    for (i = 2; i < trigger.args.length; i++) {
       phrase += ' '+trigger.args[i];
     }
     console.log('langIn:'+langIn+', phraseIn:'+phrase);
