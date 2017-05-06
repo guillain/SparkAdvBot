099c874dbba9f80130b995d48e94b7fbe1bf6b43
guillain <guillain@gmail.com>
2017-05-06 17:05:54 +0200

    Config display functions, 1.0.3

--- myConfig.js
+++ myConfig.js
@@ -9,27 +9,34 @@ var config = require('./config');
 
 exports.help = function(bot) {
   var help = '**Configuration** \n\n';
-  help += '@ [config|c] [bot] \n';
-  help += '* @ config \n';
+  help += '_Description_ Provide information related to the server, Spark and bot environments \n\n';
+  help += '_Commands_ : @ [config|c] [bot] \n\n';
+  help += '* @ config \n\n';
   help += '* @ c bot \n\n';
   bot.say(help);
 };
 
 
 exports.config = function (bot, trigger) {
-  var tosay = 'Current config: \n';
-  tosay += '**Server** \n';
-  tosay += '* port:'+config.port+'\n';
-  tosay += '* address:'+config.address+'\n';
-  tosay += '* debug:'+config.debug+'\n';
-  tosay += '**Spark** \n';
-  tosay += '* sparkbot: '+config.sparkbot+'\n';
-  tosay += '* webhookUrl: '+config.webhookUrl+'\n';
-  tosay += '* webhookRequestJSONLocation: '+config.webhookRequestJSONLocation+'\n';
-  tosay += '* removeWebhooksOnStart: '+config.removeWebhooksOnStart+'\n';
-  tosay += '* token: '+config.token+'\n';
-  tosay += '**Bot** \n';
-  tosay += '* _ToDo_ \n';
+  // Remove the first two args
+  trigger.args.splice(0,2);
+  
+  if (trigger.args['0'] == 'help') { module.exports.help(bot); }
+  else {
+    var tosay = 'Current config: \n';
+    tosay += '**Server** \n';
+    tosay += '* port:'+config.port+'\n';
+    tosay += '* address:'+config.address+'\n';
+    tosay += '* debug:'+config.debug+'\n';
+    tosay += '**Spark** \n';
+    tosay += '* sparkbot: '+config.sparkbot+'\n';
+    tosay += '* webhookUrl: '+config.webhookUrl+'\n';
+    tosay += '* webhookRequestJSONLocation: '+config.webhookRequestJSONLocation+'\n';
+    tosay += '* removeWebhooksOnStart: '+config.removeWebhooksOnStart+'\n';
+    tosay += '* token: '+config.token+'\n';
+    tosay += '**Bot** \n';
+    tosay += '* _ToDo_ \n';
 
-  bot.say(tosay);
+    bot.say(tosay);
+  }
 };
